438938a33734fff37e54bb0cc0818104dd62520c
Zinfour <zinfour17@gmail.com>
2026-09-03 09:44:48 +0200

    Add swaggerUI.

--- LMS.API/Program.cs
+++ LMS.API/Program.cs
@@ -49,13 +49,19 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // can be found at /openapi/v1.json
     app.MapOpenApi();
+    app.UseSwagger();
+    // can be found at /swagger
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
