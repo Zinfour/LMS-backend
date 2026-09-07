e6c68eb84fe0fdf72cd2aaa86af837fe5df92c5b
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-09-07 15:45:43 +0200

    create file/initial commit

---
+++ LMS.API/Controllers/ModuleController.cs
@@ -0,0 +1,11 @@
+// Teachers should be able to create, read, update and delete modules for a course.
+// Only users that are logged in and have the role Teacher should be able to perform these operations.
+
+// When creating or updating a module make sure that the start and end times are correct (start time before end time) and that they do not overlap the start or end time of other modules for the same course.
+// Also make sure that the times are within the start and end time of the course.
+
+// GET
+// POST
+// PUT
+// DELETE
+// /api/courses/{id}/modules
