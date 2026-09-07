bb77d4459ec47d92755b1f94f5d5ca20cf9cd268
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-09-07 16:03:38 +0200

    first attempt at module dto

---
+++ LMS.API/DTOs/Module/ModuleDto.cs
@@ -0,0 +1,31 @@
+// namespace LMS.API.Models
+// {
+//     public class Module
+//     {
+//         public int Id { get; set; }
+//         public DateTime CreatedAt { get; set; }
+//         public DateTime UpdatedAt { get; set; }
+
+//         public string Name { get; set; } = null!;
+//         public string Description { get; set; } = null!;
+//         public DateOnly StartDate { get; set; }
+//         public DateOnly EndDate { get; set; }
+//         public string? ImageURL { get; set; }
+//         public List<Activity> Activities { get; set; } = [];
+//         public List<ModuleResource> Resources { get; set; } = [];
+        
+//         public int CourseId { get; set; }
+//         public Course Course { get; set; } = null!;
+//     }
+// }
+
+namespace LMS.API.DTOs;
+public class ModuleDto
+{
+    public string Name {get; set;} = null!;
+    public string Description {get; set;} = null!;
+    public DateOnly StartDate {get; set;}
+    public DateOnly EndDate {get; set;}
+    public string? ImageURL {get; set;}
+
+}
