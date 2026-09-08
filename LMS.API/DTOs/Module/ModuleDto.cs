4f30a80f45fe26e54d658db4498dd0db4b4dca9f
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-09-08 10:43:13 +0200

    include said changes in controller

--- LMS.API/DTOs/Module/ModuleDto.cs
+++ LMS.API/DTOs/Module/ModuleDto.cs
@@ -22,10 +22,11 @@
 namespace LMS.API.DTOs;
 public class ModuleDto
 {
+    public int Id {get; set;}
     public string Name {get; set;} = null!;
     public string Description {get; set;} = null!;
     public DateOnly StartDate {get; set;}
     public DateOnly EndDate {get; set;}
     public string? ImageURL {get; set;}
-
+    public int CourseId {get; set;}
 }
