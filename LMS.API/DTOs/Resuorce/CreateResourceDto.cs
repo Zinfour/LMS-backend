91c54aabc3462f54e3bd877215c8ee994de0de16
Amer Mauweyah <konto1101@outlook.com>
2026-09-10 07:03:48 +0200

    Add endpoints to manage course resources

---
+++ LMS.API/DTOs/Resuorce/CreateResourceDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS.API.DTOs.Resuorce
+{
+    public class CreateResourceDto
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedByUserId { get; set; } = null!;
+        public string? URL { get; set; }
+        public string ResourceType { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
