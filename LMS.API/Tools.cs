13f4592e7c52a80d36e7c885f0e58040158dd1d2
Amer Mauweyah <konto1101@outlook.com>
2026-09-10 08:20:31 +0200

    Add module resource endpoints

---
+++ LMS.API/Tools.cs
@@ -0,0 +1,33 @@
+﻿using LMS.API.Models.Resources;
+
+namespace LMS.API
+{
+    public static class Tools
+    {
+        public static ResourceType ParseResourceType(string resourceType)
+        {
+            return resourceType.ToLower() switch
+            {
+                "instruction" => ResourceType.Instruction,
+                "textmaterial" => ResourceType.TextMaterial,
+                "link" => ResourceType.Link,
+                "summary" => ResourceType.Summary,
+                "reference" => ResourceType.Reference,
+                _ => ResourceType.TextMaterial
+            };
+        }
+
+        public static string ResourceTypeToString(ResourceType resourceType)
+        {
+            return resourceType switch
+            {
+                ResourceType.Instruction => "Instruction",
+                ResourceType.TextMaterial => "TextMaterial",
+                ResourceType.Link => "Link",
+                ResourceType.Summary => "Summary",
+                ResourceType.Reference => "Reference",
+                _ => "TextMaterial"
+            };
+        }
+    }
+}
